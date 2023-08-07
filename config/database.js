0a504c79d7835d58ce408941925e3bd72915f59c
chaitanya <chaitanya.tata215@gmail.com>
2023-08-08 01:48:14 +0530

    Added cloudinary

--- config/database.js
+++ config/database.js
@@ -76,7 +76,7 @@ module.exports = ({ env }) => {
       },
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 6),
+        max: env.int("DATABASE_POOL_MAX", 10),
         acquireTimeoutMillis: 3000000,
         createTimeoutMillis: 300000,
         destroyTimeoutMillis: 300000,
